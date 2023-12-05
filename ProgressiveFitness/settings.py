cec5e76e8ba3a0717aa8b24d90e267e2ad434a9c
Jimmy Bonilla <jimmyb091202@gmail.com>
2023-12-05 00:23:56 -0800

    dj database url

--- ProgressiveFitness/settings.py
+++ ProgressiveFitness/settings.py
@@ -145,6 +145,9 @@ DATABASES = {
         'PORT': '5432',      # Set to the port used by your PostgreSQL server
     }
 }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
